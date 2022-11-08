33cb53774ab2fde27613b7884e15f1bf850de561
Antonyhyson <antonyhyson@gmail.com>
2022-11-08 09:50:24 +0530

    change

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-
+import Login from './Login';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>
 );
 
