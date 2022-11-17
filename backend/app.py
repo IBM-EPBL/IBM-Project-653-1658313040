1be87bb8c1365fd1b5cb1d6f6f435d15d31eefcf
Antonyhyson <antonyhyson@gmail.com>
2022-11-17 14:25:28 +0530

    re

--- backend/app.py
+++ backend/app.py
@@ -10,8 +10,7 @@ app = Flask(__name__)
 CORS(app)
 
 
-conn = ibm_db.connect(    
-"DATABASE=bludb;HOSTNAME=6667d8e9-9d4d-4ccb-ba32-21da3bb5aafc.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=30376;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=yzk69108;PWD=vL0zrqj9PeaSutC4",'',''
+conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=6667d8e9-9d4d-4ccb-ba32-21da3bb5aafc.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=30376;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=yzk69108;PWD=vL0zrqj9PeaSutC4",'',''
 )
 print(conn)
 print("connection successful...")
